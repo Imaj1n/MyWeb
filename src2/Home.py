b61a8b1bb1b5b6eefa10ed363feb01dcc1ebcaa9
Alief Hisyam Al Hasany MR <139447107+Imaj1n@users.noreply.github.com>
2025-01-10 21:54:43 +0700

    Update Home.py

--- src2/Home.py
+++ src2/Home.py
@@ -9,4 +9,6 @@ from streamlit_lottie import st_lottie
 import json
 
 def app():
-    st.write("Hello World")
+    url_ayang = "https://raw.githubusercontent.com/Imaj1n/MyWeb/main/src2/Bahan_Lain/Gif_Us/Gif_Ayang1.gif"
+    #Gif_Ayang1.gif
+    st.image(url_ayang,use_container_width=True)
