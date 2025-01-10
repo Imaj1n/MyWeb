7d560e960876e1c2d7c30c224c0a36bfd798ae89
Alief Hisyam Al Hasany MR <139447107+Imaj1n@users.noreply.github.com>
2025-01-10 22:08:32 +0700

    Update Home.py

--- src2/Home.py
+++ src2/Home.py
@@ -9,17 +9,7 @@ from streamlit_lottie import st_lottie
 import json
 
 def app():
-    audio = {
-            "Kill Bill - SZA": "https://raw.githubusercontent.com/Imaj1n/MyWeb/main/src2/Bahan_Lain/Music/SZA%20-%20Kill%20Bill%20(Audio).mp3",
-            "Bad - Wave to Earth": "https://raw.githubusercontent.com/Imaj1n/MyWeb/main/src2/Bahan_Lain/Music/wave%20to%20earth%20-%20bad%20(Official%20Lyric%20Video).mp3",
-            "Blue - Yung Kai": "https://raw.githubusercontent.com/Imaj1n/MyWeb/main/src2/Bahan_Lain/Music/Yung%20Kai%20-%20Blue.mp3"
-        }
-
-        # Menampilkan pilihan musik di selectbox
-    music_choice = st.selectbox("Mau Musik Apaa", list(audio.keys()), format_func=lambda x: f"{x}")
-
-        # Mengambil file path berdasarkan pilihan pengguna
+    # Mengambil file path berdasarkan pilihan pengguna
     selected_audio = "https://raw.githubusercontent.com/Imaj1n/MyWeb/main/src2/Bahan_Lain/Music/SZA%20-%20Kill%20Bill%20(Audio).mp3"
-        # Memutar audio berdasarkan pilihan
-    audio_file = (open(selected_audio, "rb")).read()
-    st.audio(audio_file, format="audio/mp3")
+    # Memutar audio berdasarkan pilihan
+    st.audio(selected_audio, format="audio/mpeg", loop=True)
